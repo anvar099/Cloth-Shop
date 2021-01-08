0fb75a016dd5a990914872eb4c0dec6f067a143d
anvar099 <abduganievanvar99@gmail.com>
2021-01-08 17:06:58 +0500

    categories page

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -7,34 +7,34 @@ const Categories = () => {
   return (
     <div className="categories-container">
       <div className="categories-row">
-          <div className="category-item-above">
+          <div className="category-item-above1">
             <div className="item-label">
               <h1 className="item-name">Hats</h1>
               <h2 className="item-text">Shop Now</h2>
             </div>
           </div>
-          <div className="category-item-above">
+          <div className="category-item-above2">
             <div className="item-label">
-              <h1 className="item-name">Hats</h1>
+              <h1 className="item-name">Jackets</h1>
               <h2 className="item-text">Shop Now</h2>
             </div>
-          </div><div className="category-item-above">
+          </div><div className="category-item-above3">
             <div className="item-label">
-              <h1 className="item-name">Hats</h1>
+              <h1 className="item-name">Sneakers</h1>
               <h2 className="item-text">Shop Now</h2>
             </div>
           </div>
       </div>
       <div className="categories-row">
-        <div className="category-item">
+        <div className="category-item1">
           <div className="item-label">
-              <h1 className="item-name">Hats</h1>
+              <h1 className="item-name">Womens</h1>
               <h2 className="item-text">Shop Now</h2>
             </div>
           </div>
-          <div className="category-item">
+          <div className="category-item2">
             <div className="item-label">
-              <h1 className="item-name">Hats</h1>
+              <h1 className="item-name">Mens</h1>
               <h2 className="item-text">Shop Now</h2>
             </div>
           </div>
